43beaa4b77b8ce972eaa46940079e8ad57108979
Igor Pedrosa <ilap1036@gmail.com>
2022-09-16 15:58:08 -0300

    build: react, jest, sass

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <img alt="" src="https://www.google.com/google.png" />
+      <button type="button">Naruto</button>
+    </>
+  );
+}
+
+export default App;
